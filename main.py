393a5e2850077c1ff89f29b7440b8b2c87845f5f
dev-manager <factorio356@gmail.com>
2022-04-15 18:05:24 +0900

    fix auto update

--- main.py
+++ main.py
@@ -107,9 +107,11 @@ async def update(ctx):
         for i in bot.commands:
             bot.remove_command(i.name)
         await ctx.channel.send("업데이트 중에는 봇 이용이 불가능합니다.")
+        db.commit()
+        db.close()
         await asyncio.sleep(10)
         sock.sendto("update".encode(), ("127.0.0.1", 8080))
-        sys.exit(1)
+        await bot.close()
     else:
         await ctx.channel.send("동건맨 아니면 업뎃 못함 ㅅㄱ")
 
