b34f47e7e242f02f2499f164860182f725cd7951
dev-manager <factorio356@gmail.com>
2022-04-15 17:56:22 +0900

    fix auto update

--- main.py
+++ main.py
@@ -106,7 +106,6 @@ async def update(ctx):
         await ctx.channel.send("모든 명령어를 삭제 중입니다.")
         for i in bot.commands:
             bot.remove_command(i.name)
-        await ctx.channel.send("업데이트 중입니다.")
         await ctx.channel.send("업데이트 중에는 봇 이용이 불가능합니다.")
         await asyncio.sleep(10)
         sock.sendto("update".encode(), ("127.0.0.1", 8080))
