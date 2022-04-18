3dfcfbb0a9086fb6158a320a418e5ae3be071f85
doksuri761 <factorio356@gmail.com>
2022-04-18 17:46:10 +0900

    결정 기능 추가

--- funcs/decision.py
+++ funcs/decision.py
@@ -2,6 +2,9 @@ import random
 
 
 async def decider(ctx):
-    product = ctx.message.content.replace("!살까말까 ", "")
-    msg = [f"민수 : {product}은(는) 당신을 원하고 있습니다.", "리하: 이눔!!! 그만사!!"]
-    await ctx.channel.send(random.choice(msg))
+    if ctx.message.content == "!살까말까":
+        await ctx.channel.send("인수 낸놔!")
+    else:
+        product = ctx.message.content.replace("!살까말까 ", "")
+        msg = [f"민수 : {product}은(는) 당신을 원하고 있습니다.", "리하: 이눔!!! 그만사!!"]
+        await ctx.channel.send(random.choice(msg))
