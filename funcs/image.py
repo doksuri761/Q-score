e7a91eb02a9bfa40c2b9baff7641204767db722c
doksuri761 <factorio356@gmail.com>
2022-04-19 22:17:52 +0900

    멘션기능 추가

--- funcs/image.py
+++ funcs/image.py
@@ -2,7 +2,7 @@ import random
 
 
 async def up(ctx, cur, db):
-    nick = ctx.message.content.replace("!떡상 ", "")[2:-1]
+    nick = int(ctx.message.content.replace("!떡상 ", "")[2:-1])
     if nick != ctx.author.id:
         sql = f"select * from user where(user_id=\"{ctx.author.display_name}\")"
         cur.execute(sql)
@@ -17,13 +17,13 @@ async def up(ctx, cur, db):
             sql = f"update user set image={query[0] + 1} where(user_id=\"{nick}\")"
             cur.execute(sql)
             db.commit()
-            await ctx.channel.send(nick + "님의 이미지가 1 떡상했어요.")
+            await ctx.channel.send(query[1] + "님의 이미지가 1 떡상했어요.")
     else:
         await ctx.channel.send("님 양심 없?")
 
 
 async def down(ctx, cur, db):
-    nick = ctx.message.content.replace("!떡락 ", "")[2:-1]
+    nick = int(ctx.message.content.replace("!떡락 ", "")[2:-1])
     if nick != ctx.author.id:
         sql = f"select * from user where(user_id=\"{ctx.author.display_name}\")"
         cur.execute(sql)
@@ -39,32 +39,32 @@ async def down(ctx, cur, db):
                 sql = f"update user set image={query[0] - 1} where(user_id=\"{nick}\")"
                 cur.execute(sql)
                 db.commit()
-                await ctx.channel.send(nick + "님의 이미지가 80% 확률에 의하여 1 떡락했어요.")
+                await ctx.channel.send(query[1] + "님의 이미지가 80% 확률에 의하여 1 떡락했어요.")
             elif 91 > amount < 81:
                 sql = f"update user set image={query[0] - 2} where(user_id=\"{nick}\")"
                 cur.execute(sql)
                 db.commit()
-                await ctx.channel.send(nick + "님의 이미지가 10% 확률에 의하여 2 떡락했어요.")
+                await ctx.channel.send(query[1] + "님의 이미지가 10% 확률에 의하여 2 떡락했어요.")
             elif 96 > amount > 91:
                 sql = f"update user set image={query[0] - 3} where(user_id=\"{nick}\")"
                 cur.execute(sql)
                 db.commit()
-                await ctx.channel.send(nick + "님의 이미지가 5% 확률에 의하여 3 떡락했어요.")
+                await ctx.channel.send(query[1] + "님의 이미지가 5% 확률에 의하여 3 떡락했어요.")
             elif 99 > amount > 96:
                 sql = f"update user set image={query[0] - 4} where(user_id=\"{nick}\")"
                 cur.execute(sql)
                 db.commit()
-                await ctx.channel.send(nick + "님의 이미지가 3% 확률에 의하여 4 떡락했어요.")
+                await ctx.channel.send(query[1] + "님의 이미지가 3% 확률에 의하여 4 떡락했어요.")
             elif amount == 100:
                 sql = f"update user set image={query[0] - 5} where(user_id=\"{nick}\")"
                 cur.execute(sql)
                 db.commit()
-                await ctx.channel.send(nick + "님의 이미지가 1% 확률에 의하여 5 떡락했어요.")
+                await ctx.channel.send(query[1] + "님의 이미지가 1% 확률에 의하여 5 떡락했어요.")
             else:
                 sql = f"update user set image={query[0] - 1} where(user_id=\"{nick}\")"
                 cur.execute(sql)
                 db.commit()
-                await ctx.channel.send(nick + "님의 이미지가 80% 확률에 의하여 1 떡락했어요.")
+                await ctx.channel.send(query[1] + "님의 이미지가 80% 확률에 의하여 1 떡락했어요.")
     else:
         await ctx.channel.send("님 양심 없?")
 
